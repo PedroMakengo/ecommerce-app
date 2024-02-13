c75cc4e2d3a9af75c883263b37e7c1ea428cf996
PedroMakengo <pedromakengomiguel@gmail.com>
2024-02-13 23:59:04 +0100

    Tratamento de erros no projeto

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,5 +1,60 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
+import { prismaClient } from '../index'
+import { hashSync, compareSync } from 'bcrypt'
+import * as jwt from 'jsonwebtoken'
+import { JWT_SECRET } from '../secrets'
+import { BadRequestsException } from '../exceptions/bad-requests'
+import { ErrorCode } from '../exceptions/root'
 
-export const login = (request: Request, response: Response) => {
-  response.send('Login works')
+// Criando um usuário
+export const signup = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const { email, password, name } = request.body
+
+  let user = await prismaClient.user.findFirst({ where: { email } })
+
+  if (user) {
+    next(
+      new BadRequestsException(
+        'User already exists!',
+        ErrorCode.USER_ALREADY_EXISTS
+      )
+    )
+  }
+
+  user = await prismaClient.user.create({
+    data: {
+      name,
+      email,
+      password: hashSync(password, 10),
+    },
+  })
+
+  response.json(user)
+}
+
+// Login
+export const login = async (request: Request, response: Response) => {
+  const { email, password } = request.body
+
+  let user = await prismaClient.user.findFirst({ where: { email } })
+
+  if (!user) {
+    throw Error('User does not exits!')
+  }
+
+  if (!compareSync(password, user.password)) {
+    throw Error('Incorrect password!')
+  }
+
+  const token = jwt.sign(
+    {
+      userId: user.id,
+    },
+    JWT_SECRET
+  )
+  response.json({ user, token })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import express, { Express, Request, Response } from 'express'
 import { PORT } from './secrets'
 import rootRouter from './routes'
+import { PrismaClient } from '@prisma/client'
+import { errorMiddleware } from './middlewares/errors'
 
 const app: Express = express()
 
+app.use(express.json())
+// Minhas rotas
 app.use('/api', rootRouter)
+
+// Usando o meu prisma
+export const prismaClient = new PrismaClient({
+  log: ['query'],
+})
+
+// Middleware para verificar os erros
+app.use(errorMiddleware)
 
 app.listen(PORT, () => console.log('Server running to port 3000'))

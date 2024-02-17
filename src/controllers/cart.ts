f48c47f91bf8b67c52d3d05e4fcaddf2948128f6
PedroMakengo <pedromakengomiguel@gmail.com>
2024-02-17 11:43:10 +0100

    Adicionado a rotas para carts

---
+++ src/controllers/cart.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express'
+
+export const addItemToCart = async (request: Request, response: Response) => {}
+
+export const deleteItemFromCart = async (
+  request: Request,
+  response: Response
+) => {}
+
+export const changeQuantity = async (request: Request, response: Response) => {}
+
+export const getCart = async (request: Request, response: Response) => {}
---
+++ src/routes/cart.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import authMiddleware from '../middlewares/auth'
+import { errorHandler } from '../error-handler'
+import {
+  addItemToCart,
+  changeQuantity,
+  deleteItemFromCart,
+  getCart,
+} from '../controllers/cart'
+
+export const cartRoutes = Router()
+
+cartRoutes.post('/', [authMiddleware], errorHandler(addItemToCart))
+cartRoutes.get('/:id', [authMiddleware], errorHandler(getCart))
+cartRoutes.delete('/:id', [authMiddleware], errorHandler(deleteItemFromCart))
+cartRoutes.put('/:id', [authMiddleware], errorHandler(changeQuantity))
